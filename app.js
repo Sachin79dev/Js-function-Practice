d91c8b0c80df916227ea8bf9fb3649a58ba023f7
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-05-26 23:35:44 +0530

    function for finding power of recursive
    Think about power like this:
    
    2^4 = 2 × 2 × 2 × 2
    
    You can rewrite it as:
    
    2^4 = 2 × 2^3
    
    So the recursive pattern becomes:
    
    power(base, exp) = base * power(base, exp - 1)
    
    
    Important part → Base Case
    
    Recursion must stop.
    
    Think:
    
    2^0 = 1
    
    So when exponent becomes 0, return 1.
    
    Structure idea
    
    
    function power(base, exp) {
    
       // base case
    
       // recursive case
    
    }
    
    Recursive line idea:
    
    return base * power(base, exp - 1);
    
    
    Example flow
    
    
    power(2, 3)
    
    = 2 * power(2, 2)
    
    = 2 * (2 * power(2, 1))
    
    = 2 * (2 * (2 * power(2, 0)))
    
    = 2 * 2 * 2 * 1
    
    = 8

--- app.js
+++ app.js
@@ -289,3 +289,18 @@ function fib(n) {
 
 
 console.log(fib(8));
+
+
+
+
+//function for finding power of recursive
+function power(base, exp) {
+    if (exp === 0) {
+        return 1;
+    } else {
+        return base * power(base, exp - 1);
+    }
+}
+
+
+console.log(power(2, 4));
