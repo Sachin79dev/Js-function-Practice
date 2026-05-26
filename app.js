08f8514411420878ccd9c8880dffd8c4734665bf
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-05-26 23:25:37 +0530

    recursive fibonacci function
    Fibonacci numbers are a sequence where each number is the sum of the two preceding ones. Starting from 0 and 1, the sequence progresses infinitely as: \(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, \dots\)
    
    
    
    
    Recursive part
    fib(n - 1) + fib(n - 2)
    
    means:
    
    add previous two Fibonacci numbers
    
    Example:
    
    fib(5)
    = fib(4) + fib(3)
    
    = 3 + 2
    
    = 5

--- app.js
+++ app.js
@@ -271,3 +271,21 @@ function factorial(n) {
 
 
 console.log(factorial(5));
+
+
+
+
+
+// recursive fibonacci function
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+
+console.log(fib(8));
